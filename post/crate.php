f8913467d594515bc8016e66fbce69884fe6cd09
De-Dit <raditdwiputra48@gmail.com>
2025-12-26 12:22:09 +0800

    Membuat crate

--- post/crate.php
+++ post/crate.php
@@ -1,1 +1,13 @@
-pp
+<?php
+include "../config/database.php";
+
+if (isset($_POST['submit'])) {
+    $judul  = mysqli_real_escape_string($conn, $_POST['judul']);
+    $konten = mysqli_real_escape_string($conn, $_POST['konten']);
+
+    if ($judul != "" && $konten != "") {
+        mysqli_query($conn, "INSERT INTO posts (judul, konten) VALUES ('$judul', '$konten')");
+        header("Location: ../index.php");
+    }
+}
+?>
